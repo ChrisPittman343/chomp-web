2359efca10df225b1ef4658adfd74afa68dd5e58
ChrisPittman <51276178+ChrisPittman343@users.noreply.github.com>
2020-12-25 20:08:44 -0500

    Added footer

---
+++ src/components/_common/Footer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Footer.css";
+
+interface Props {}
+
+export const Footer = (props: Props) => {
+  return (
+    <footer className="page-footer">
+      <ul className="small-txt">
+        <li>© 2021 Chomp</li>
+        <li>
+          <a href="/">Terms</a>
+        </li>
+        <li>
+          <a href="/">Privacy</a>
+        </li>
+        <li>
+          <a href="/">Github</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
