90075f3b63b0a0508124bc624785dab233c5a103
ChrisPittman <51276178+ChrisPittman343@users.noreply.github.com>
2020-12-28 20:09:29 -0500

    Created AuthRoute components outline

---
+++ src/components/_common/routes/BasicAuthRoute.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Route, RouteProps } from "react-router-dom";
+import { UserContext } from "../../../contexts";
+import { signInWithGoogle } from "../../../utils/signInWithGoogle";
+import { Spinner } from "../Spinner";
+
+interface Props {
+  children: any;
+  path: string;
+  routeProps?: RouteProps;
+}
+
+export const BasicAuthRoute = (props: Props) => {
+  const { user, loading, error } = useContext(UserContext)!;
+  let content;
+  if (loading) {
+    content = (
+      <Spinner parentSize={70} style={{ margin: "auto", marginTop: 70 }} />
+    );
+  } else if (error || !user) {
+    signInWithGoogle();
+    content = <></>;
+  } else {
+    content = props.children;
+  }
+  return <Route {...props.routeProps} path={props.path} children={content} />;
+};
---
+++ src/components/_common/routes/TeacherAuthRoute.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RouteProps } from "react-router-dom";
+
+interface Props {
+  children: any;
+  path: string;
+  routeProps?: RouteProps;
+}
+
+export const TeacherAuthRoute = (props: Props) => {
+  return <div></div>;
+};
